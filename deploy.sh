d8404b7226b722e18402fb2f93ef8e7667b30117
Jacksgong <igzhenjie@gmail.com>
2018-12-09 17:09:53 +0800

    ci: update deploy for automatically deploy on server

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,19 @@
 # @version 1.0.0
 #
 # For deplay public folder.
+set -e
 
-echo "$(tput setaf 3)---------Public-Release v2.0--------- $(tput sgr 0)"
+echo "$(tput setaf 3)---------Public-Release v3.0--------- $(tput sgr 0)"
 
+echo "$(tput setaf 3)>>>>>>start generate blog$(tput sgr 0)"
+hexo g
+echo "$(tput setaf 3)<<<<<<finish generate blog$(tput sgr 0)"
+
+echo "$(tput setaf 3)>>>>>>start push source code$(tput sgr 0)"
 bash push-double-end.sh
+echo "$(tput setaf 3)<<<<<<finish push source code$(tput sgr 0)"
+
+echo "$(tput setaf 3)>>>>>>start push blog$(tput sgr 0)"
 cd public
 rm -rf .git
 git init
@@ -21,4 +30,13 @@ git commit -m "$commit_msg"
 
 # Push:
 git push --force origin master
-echo "$(tput setaf 3)Has successfully cimmit: $commit_msg$(tput sgr 0)"
+echo "$(tput setaf 3)>>>>>>fninsh push blog$(tput sgr 0)"
+
+# add marker
+echo "$(tput setaf 3)>>>>>>start add deploy marker$(tput sgr 0)"
+git clone git@git.jacksgong.com:Jacksgong/blog-deploy-auto-marker.git deploy-marker
+cd deploy-marker
+bash add-deploy.sh
+cd ..
+rm -rf deploy-marker
+echo "$(tput setaf 3)<<<<<<finish add deploy marker$(tput sgr 0)"
