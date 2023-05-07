a537087e4853a690c0d58fb3de4498345fbc13f1
jacks <gongzhenjie@bytedance.com>
2023-05-07 15:45:03 +0800

    ci: fix ref loop for nested link in obsidian to hexo blog script

--- obsidian2post.py
+++ obsidian2post.py
@@ -48,7 +48,7 @@ with open(ob_markdown_file_path, 'r') as file:
         target_file.write(filedata)
 
 # 处理![[post#subtitle]]这样的文章内链引用
-def replace_obsidian(filedata, path_of_obsidian_valt, parent_ref_list=[]):
+def replace_wiki_link_ref2real_content(filedata, path_of_obsidian_valt, parent_ref_list=[]):
     # find 'post' and 'subtitle' from read content like '![[post#subtitle]]'
     obsidian_content = re.findall(r'!\[\[(.*?)\]\]', filedata)
     # if not match the pattern, then skip
@@ -67,7 +67,7 @@ def replace_obsidian(filedata, path_of_obsidian_valt, parent_ref_list=[]):
         # get the post and subtitle from the obsidian
         post = obsidian_split[0]
         subtitle = obsidian_split[1]
-        parent_ref_list.append(obsidian)
+        parent_ref_list.append([post, subtitle])
 
         # find file with post.md by traversing path_of_obsidian_valt directory
         post_file_path = ''
@@ -97,6 +97,8 @@ def replace_obsidian(filedata, path_of_obsidian_valt, parent_ref_list=[]):
                     # if the content is start with '#' and end with subtitle, then begine assemble the content
                     if line.startswith('#') and line.endswith(subtitle):
                         begine_assemble = True
+                        # print with green color
+                        print('\033[32mBegin assembling content for ![[{}#{}]]\033[0m'.format(post, subtitle))
                         continue
 
                 # if begine assemble the content, then assemble the content
@@ -109,42 +111,59 @@ def replace_obsidian(filedata, path_of_obsidian_valt, parent_ref_list=[]):
                         code_block = False
 
                     # if the content is start with '#' and subtitle_level is same or higher than subtitle_level, then end assemble the content
-                    if line.startswith('#') and len(line.split(' ')[0]) <= subtitle_level and not code_block:
-                        break
+                    if not code_block and line.startswith('#') and len(line.split(' ')) >= 2:
+                        if len(line.split(' ')[0]) <= subtitle_level:
+                            break
+                        # with red color for print this line
+                        print('\033[31m{}\033[0m'.format(line))
+
+                        # 为了规避类似于[[post#subtitle]]引用被使用，将所有引用内容中的post+subtitle都添加进要排除的队列
+                        curent_subtitle = line.split(' ')[1]
+                        parent_ref_list.append([post, curent_subtitle])
+
                     # if this line contains a nested loop, skip it
                     if skip_line_with_loop:
                         skip_line_with_loop = False
                         continue
 
                     # 避免循环依赖，如果当前行包含父级引用，则跳过
+                    need_ignore_because_of_parent_ref = False
                     for parent_ref in parent_ref_list:
-                        if parent_ref in line:
-                            print('parent_ref({}) in the line({}), so ignore line'.format(parent_ref, line))
-                            continue
+                        current_post = parent_ref[0]
+                        current_subtitle = parent_ref[1]
 
-                    subtitle_content += line + '\n'
+                        if '[[{}#{}]]'.format(current_post, current_subtitle) in line:
+                            # print with blue color
+                            print('\033[34mparent_ref({}) in the line({}), so ignore line\033[0m'.format(parent_ref, line))
+                            need_ignore_because_of_parent_ref = True
+                            break
+                        
+                    if not need_ignore_because_of_parent_ref:
+                        subtitle_content += line + '\n'
 
             # if not match the subtitle, then skip
             if len(subtitle_content) == 0:
                 continue
 
             # 处理当前层级的引用
-            replaced_subtitle_content = replace_obsidian(subtitle_content, path_of_obsidian_valt, parent_ref_list)
+            replaced_subtitle_content = replace_wiki_link_ref2real_content(subtitle_content, path_of_obsidian_valt, parent_ref_list)
 
             # 将target_markdown_file_path中的![[post#subtitle]]替换为subtitle_content
+            # print with yellow color
+            print('\033[33m将target_markdown_file_path中的![[{}#{}]]替换为subtitle_content({})\033[0m'.format(post, subtitle, len(replaced_subtitle_content)))
             replaced_filedata = replaced_filedata.replace('![[{}#{}]]'.format(post, subtitle), replaced_subtitle_content)
 
     return replaced_filedata
 
 
 # read each line from target_markdown_file_path and replace all obsidian content links in target_markdown_file_path like ![[post#subtitle]] to target real content in the post file and rewrite the target_markdown_file_path
-print('read each line from $target_markdown_file_path and replace all obsidian content links in target_markdown_file_path like ![[post#subtitle]] to target real content in the post file and rewrite the target_markdown_file_path')
+print('处理类似 ![[post#subtitle]] 这样的引用')
 with open(target_markdown_file_path, 'r') as file:
     # read each line from file
     filedata = file.read()
     
     # replace all obsidian content links in target_markdown_file_path like ![[post#subtitle]] to target real content in the post file and rewrite the target_markdown_file_path
-    filedata = replace_obsidian(filedata, path_of_obsidian_valt)
+    filedata = replace_wiki_link_ref2real_content(filedata, path_of_obsidian_valt)
 
     # write filedata to the target markdown file
     with open(target_markdown_file_path, 'w') as file:
@@ -175,7 +194,8 @@ with open(target_markdown_file_path, 'r') as file:
 
         # if the target asset file exist, then skip
         if os.path.exists('{}/{}'.format(target_asset_folder, target_asset_file_name)):
-            print('target asset file({}) exist, skip'.format(target_asset_file_name))
+            # print with blue color
+            print('\033[34m{} already exist, so skip\033[0m'.format(target_asset_file_name))
             continue
 
         # copy the asset to the target asset folder
@@ -212,7 +232,8 @@ with open(target_markdown_file_path, 'r') as file:
     # replace the assets path in the markdown file
     for i, asset in enumerate(assets):
         # replace the asset path in the markdown file
-        print('replacing {} to {}'.format(asset, using_assets[i]))
+        # print with green color
+        print('\033[32mreplacing {} to {}\033[0m'.format(asset, using_assets[i]))
         filedata = filedata.replace('![[{}]]'.format(asset), '![](/img/{})'.format(using_assets[i]))
 
     # write filedata to the target markdown file
